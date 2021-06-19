3a5406b173129cdf48697930abb2f37925c84e8d
Diego Ricardo <diegoricardoweb@gmail.com>
2021-06-18 22:21:55 -0300

    Creating a reponsiveness helper component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,15 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
+  typescript: {
+    reactDocgen: 'react-docgen'
+  },
   babel: async (options) => ({
     ...options,
-    plugins: [...options.plugins,  require.resolve('@babel/plugin-transform-react-jsx')]
+    plugins: [
+      ...options.plugins,
+      require.resolve('@babel/plugin-transform-react-jsx')
+    ]
   }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
