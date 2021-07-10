64a37a3c256ea5137051fa4098b6199e0cb1e079
Diego Ricardo <diegoricardoweb@gmail.com>
2021-07-09 21:26:13 -0300

    Add showcase

---
+++ src/components/Showcase/test.tsx
@@ -0,0 +1,28 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+import Showcase from '.'
+
+const props = {
+  title: 'Most Popular',
+  highlight: highlightMock,
+  games: gamesMock.slice(0, 1)
+}
+
+describe('<Showcase />', () => {
+  it('should render full showcase', () => {
+    renderWithTheme(<Showcase {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: highlightMock.title })
+    ).toBeInTheDocument()
+  })
+})
