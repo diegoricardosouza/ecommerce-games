109bd213183b95bb95672988816da59945c74fef
Diego Ricardo <diegoricardoweb@gmail.com>
2021-07-09 21:35:25 -0300

    Add showcase tests

--- src/components/Showcase/test.tsx
+++ src/components/Showcase/test.tsx
@@ -24,5 +24,46 @@ describe('<Showcase />', () => {
     expect(
       screen.getByRole('heading', { name: highlightMock.title })
     ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: gamesMock[0].title })
+    ).toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(
+      <Showcase games={props.games} highlight={props.highlight} />
+    )
+
+    screen.getByRole('heading', { name: highlightMock.title })
+    screen.getByRole('heading', { name: gamesMock[0].title })
+
+    expect(
+      screen.queryByRole('heading', { name: /most popular/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render without highlight', () => {
+    renderWithTheme(<Showcase title={props.title} games={props.games} />)
+
+    screen.getByRole('heading', { name: /most popular/i })
+    screen.getByRole('heading', { name: gamesMock[0].title })
+
+    expect(
+      screen.queryByRole('heading', { name: highlightMock.title })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render without games', () => {
+    renderWithTheme(
+      <Showcase title={props.title} highlight={props.highlight} />
+    )
+
+    screen.getByRole('heading', { name: /most popular/i })
+    screen.getByRole('heading', { name: highlightMock.title })
+
+    expect(
+      screen.queryByRole('heading', { name: gamesMock[0].title })
+    ).not.toBeInTheDocument()
   })
 })
