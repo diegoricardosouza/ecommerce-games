4cbba4c7dacfeb145540f49228ebdabbcd882cb9
Diego Ricardo <diegoricardoweb@gmail.com>
2021-07-14 17:46:59 -0300

    Add Gallery to Game page

--- src/templates/Game/_test.tsx
+++ src/templates/Game/_test.tsx
@@ -6,9 +6,7 @@ import Game from '.'
 describe('<Game />', () => {
   it('should render the heading', () => {
     renderWithTheme(<Game />)
-
     // expect(screen.getByRole('heading', { name: /Game/i })).toBeInTheDocument()
-
     // expect(container.firstChild).toMatchSnapshot()
   })
 })
