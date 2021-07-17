d8b6d59c3382ec562d3898369a3f756744573c9b
Diego Ricardo <diegoricardoweb@gmail.com>
2021-07-17 00:13:09 -0300

    Add PaymentInfo to GameItem

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -25,13 +25,22 @@ describe('<GameItem />', () => {
     expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
   })
 
-  it('should render the item with download link', () => {
-    const downloadLink = 'https://link'
+  it('should render the payment info', () => {
+    const paymentInfo = {
+      flag: 'mastercard',
+      img: '/img/mastercard.png',
+      number: '**** **** **** 4326',
+      purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+    }
 
-    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+    renderWithTheme(<GameItem {...props} paymentInfo={paymentInfo} />)
 
-    expect(
-      screen.getByRole('link', { name: `Get ${props.title} here` })
-    ).toHaveAttribute('href', downloadLink)
+    expect(screen.getByRole('img', { name: paymentInfo.flag })).toHaveAttribute(
+      'src',
+      paymentInfo.img
+    )
+
+    expect(screen.getByText(paymentInfo.number)).toBeInTheDocument()
+    expect(screen.getByText(paymentInfo.purchaseDate)).toBeInTheDocument()
   })
 })
