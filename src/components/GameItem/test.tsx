d51de4e1deeb9683f11998c5f7bbb64aabe4abc8
Diego Ricardo <diegoricardoweb@gmail.com>
2021-08-12 18:42:46 -0300

    Add overlay to Dropdown

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -25,6 +25,16 @@ describe('<GameItem />', () => {
     expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
   })
 
+  it('should render the item with download link', () => {
+    const downloadLink = 'https://link'
+
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
+
   it('should render the payment info', () => {
     const paymentInfo = {
       flag: 'mastercard',
