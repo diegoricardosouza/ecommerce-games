280f431ecdf55b127658a2ebf50c1af49726c679
Diego Ricardo <diegoricardoweb@gmail.com>
2021-08-26 22:24:17 -0300

    Fix tests after NextAuth

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,30 +1,64 @@
+import { useState } from 'react'
+import { signIn } from 'next-auth/client'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Email, Lock } from '@styled-icons/material-outlined'
+
+import { FormWrapper, FormLink, FormLoading } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-import Link from 'next/link'
-import * as S from './styles'
-import { FormWrapper, FormLink } from 'components/Form'
 
-const FormSignIn = () => (
-  <FormWrapper>
+import * as S from './styles'
+
+const FormSignIn = () => {
+  const [values, setValues] = useState({})
+  const [loading, setLoading] = useState(false)
+  const { push } = useRouter()
+
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setLoading(true)
+
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: '/'
+    })
+
+    if (result?.url) {
+      return push(result?.url)
+    }
+
+    setLoading(false)
+
+    console.error('email ou senha inválida')
+  }
+
+  return (
     <FormWrapper>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
           name="email"
           placeholder="Email"
           type="email"
+          onInputChange={(v) => handleInput('email', v)}
           icon={<Email />}
         />
         <TextField
           name="password"
           placeholder="Password"
           type="password"
+          onInputChange={(v) => handleInput('password', v)}
           icon={<Lock />}
         />
         <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
 
-        <Button size="large" fullWidth>
-          Sign in now
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Sign in now</span>}
         </Button>
 
         <FormLink>
@@ -35,7 +69,7 @@ const FormSignIn = () => (
         </FormLink>
       </form>
     </FormWrapper>
-  </FormWrapper>
-)
+  )
+}
 
 export default FormSignIn
