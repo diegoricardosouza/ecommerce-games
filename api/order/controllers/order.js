71d2c730608564bcd418d62f72e7f09b72f01a83
Diego Ricardo <diegoricardoweb@gmail.com>
2021-11-18 20:53:25 -0300

    Search Cart Game info

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -6,7 +6,23 @@
  */
 
 module.exports = {
-  createPaymentIntent: () => {
-    return "Hello World!";
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = []
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validateGame = await strapi.services.game.findOne({
+          id: game.id
+        })
+
+        if(validateGame) {
+          games.push(validateGame);
+        }
+      })
+    );
+
+    return games;
   }
 };
