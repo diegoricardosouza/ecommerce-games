98622f473b9640a158990b9b773cbd8ac434feea
Diego Ricardo <diegoricardoweb@gmail.com>
2021-12-18 00:46:10 -0300

    Add data-cy to Showcase

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -7,5 +7,9 @@ describe('Home Page', () => {
 
     // selecionou os banners
     cy.shouldRenderBanner()
+    cy.shouldRenderShowcase({ name: "New Games" })
+    cy.shouldRenderShowcase({ name: "Most Popular Games" })
+    cy.shouldRenderShowcase({ name: "Upcoming Games" })
+    cy.shouldRenderShowcase({ name: "Free Games" })
   });
 });
