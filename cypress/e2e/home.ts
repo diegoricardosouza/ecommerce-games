60f4c8f9f118f33e83dd2238fbd5137b258b3dbc
Diego Ricardo <diegoricardoweb@gmail.com>
2021-12-18 01:28:43 -0300

    Refactor to create getByDataCy custom command

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -9,7 +9,6 @@ describe('Home Page', () => {
     cy.shouldRenderBanner()
     cy.shouldRenderShowcase({ name: "New Games", highlight: false })
     cy.shouldRenderShowcase({ name: "Most Popular Games", highlight: true })
-    cy.shouldRenderShowcase({ name: "Upcoming Games", highlight: true })
     cy.shouldRenderShowcase({ name: "Free Games", highlight: true })
   });
 });
