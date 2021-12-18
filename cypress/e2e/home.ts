f5b904ea434b7645a4ddaddb92a928e2ad31c973
Diego Ricardo <diegoricardoweb@gmail.com>
2021-12-18 00:57:30 -0300

    Search for highlight inside showcase

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -7,9 +7,9 @@ describe('Home Page', () => {
 
     // selecionou os banners
     cy.shouldRenderBanner()
-    cy.shouldRenderShowcase({ name: "New Games" })
-    cy.shouldRenderShowcase({ name: "Most Popular Games" })
-    cy.shouldRenderShowcase({ name: "Upcoming Games" })
-    cy.shouldRenderShowcase({ name: "Free Games" })
+    cy.shouldRenderShowcase({ name: "New Games", highlight: false })
+    cy.shouldRenderShowcase({ name: "Most Popular Games", highlight: true })
+    cy.shouldRenderShowcase({ name: "Upcoming Games", highlight: true })
+    cy.shouldRenderShowcase({ name: "Free Games", highlight: true })
   });
 });
