0e5c7f9db188e6a1174ef24ad294709aa505c2fa
Diego Ricardo <diegoricardoweb@gmail.com>
2021-12-30 15:55:06 -0300

    Add tests to sort by platform and genre and empty match

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -17,13 +17,13 @@ describe('Explore Page', () => {
     cy.getFields(priceFields)
     cy.getFields(platformFields)
     cy.getFields(genreFields)
-  });
+  })
 
   it('should show 15 games and show more games when show more is clicked', () => {
     cy.getByDataCy('game-card').should('have.length', 15)
     cy.findByRole('button', { name: /show more/i }).click()
     cy.getByDataCy('game-card').should('have.length', 30)
-  });
+  })
 
   it('should order by price', () => {
     cy.findByText(/lowest to highest/i).click()
@@ -39,5 +39,30 @@ describe('Explore Page', () => {
     cy.getByDataCy('game-card').first().within(() => {
       cy.findByText('R$ 0,00').should('not.exist')
     })
-  });
-});
+  })
+
+  it('should filter by platform and genre', () => {
+    cy.findByText(/windows/i).click()
+    cy.location('href').should('contain', 'platforms=windows')
+
+    cy.findByText(/linux/i).click()
+    cy.location('href').should('contain', 'platforms=linux')
+
+    cy.findByText(/mac os/i).click()
+    cy.location('href').should('contain', 'platforms=mac')
+
+    cy.findByText(/action/i).click()
+    cy.location('href').should('contain', 'categories=action')
+  })
+
+  it('should return empty when no games match', () => {
+    cy.visit('/games')
+
+    cy.findByText(/free/i).click()
+    cy.findByText(/linux/i).click()
+    cy.findByText(/sports/i).click()
+
+    cy.getByDataCy('game-card').should('not.exist')
+    cy.findByText(/We didn't find any games with this filter/i).should('exist')
+  })
+})
