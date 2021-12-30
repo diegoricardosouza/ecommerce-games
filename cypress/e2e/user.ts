05f9fccda6a178440295d9933c3a562503d99942
Diego Ricardo <diegoricardoweb@gmail.com>
2021-12-30 17:28:46 -0300

    Add SignIn custom command

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -15,11 +15,11 @@ describe('User', () => {
   })
 
   it('should sign in and sign out', () => {
-    cy.visit('sign-in')
+    cy.visit('/sign-in')
 
-    cy.findByPlaceholderText(/email/i).type('e2e@wongames.com')
-    cy.findByPlaceholderText(/^password/i).type('123456')
-    cy.findByRole('button', { name: /sign in now/i }).click()
+    cy.signIn()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
 
     cy.findByText(/cypress/i).should('exist').click()
     cy.findByText(/sign out/i).click()
