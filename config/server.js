cc27189facdee8a1394c853e43841377edde7f81
Diego Ricardo <diegoricardoweb@gmail.com>
2022-01-02 00:48:06 -0300

    Adjusting project

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '2bf20fd255565e4e3f7bfb60f5837c41'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'a9e6c8df-cd5e-49ae-8f8c-096efa1b22d4'
-};
